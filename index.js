326a91c1a3ab62fd638dd1ebfe7500023d862f6c
premgc2025@gmail.com <premgc2025@gmail.com>
2024-10-24 14:14:50 +0545

    CORS CONFIG

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ import mongoose from 'mongoose'
 
 
 const app = express()
-app.use(cors())
-// const corsOptions = {
-//     origin: '*', // Replace with your frontend URL
-//     methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
-//   };
+
+const corsOptions = {
+    origin: '*', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
+  };
   
-//   app.use(cors(corsOptions));
+  app.use(cors(corsOptions));
 
 app.use(express.json())
 // app.use(express.static('/tmp'))
