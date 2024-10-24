03b15c2e5a39f2ffba2a7562baebd939e9f727a6
premgc2025@gmail.com <premgc2025@gmail.com>
2024-10-24 12:42:11 +0545

    Installed CORS

--- Controller/EmployeeController.js
+++ Controller/EmployeeController.js
@@ -14,7 +14,7 @@ import {CloudinaryStorage} from 'multer-storage-cloudinary'
  cloudinary.config({ 
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
     api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+    api_secret:process.env.CLOUDINARY_API_SECRET, //  'View API Keys' above to copy your API secret
 });
 
 
